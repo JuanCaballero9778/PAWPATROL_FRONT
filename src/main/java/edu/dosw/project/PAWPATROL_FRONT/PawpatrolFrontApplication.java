173bbda5d2585befa37c8cc2117b8a3d9d11bd7b
Juan Pablo Caballero Castellanos <juan.ccastellanos@mail.escuelaing.edu.co>
2025-09-05 15:29:09 -0500

    Semana_4 - Juan Caballero : Estructura de carpetas

---
+++ src/main/java/edu/dosw/project/PAWPATROL_FRONT/PawpatrolFrontApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.project.PAWPATROL_FRONT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawpatrolFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawpatrolFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/project/PAWPATROL_FRONT/PawpatrolFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.project.PAWPATROL_FRONT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawpatrolFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
